189e0f78fc72a2b4fb9e8a5d66f9077887e92b16
MarriccZwei <marricc7@gmail.com>
2025-10-02 18:06:33 -0700

    Revert "mickiewicz api"
    This reverts commit 1dfea137308393a84b5185eb5d0111fb14d635eb.

--- frontend/app/src/main/java/com/cpen321/usermanagement/data/remote/wl/MickiewiczWL.kt
+++ frontend/app/src/main/java/com/cpen321/usermanagement/data/remote/wl/MickiewiczWL.kt
@@ -5,7 +5,6 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
-import android.util.Log
 
 
 object MickiewiczWL {
@@ -31,7 +30,7 @@ object MickiewiczWL {
     private val mickiewiczInterface: MickiewiczInterface = retrofit.create(MickiewiczInterface::class.java)
 
     var selectedPoem = MickiewiczPoem()
-    private var poemList = listOf<MickiewiczPoem>(MickiewiczPoem())
+    private var poemList = listOf<MickiewiczPoem>(selectedPoem)
 
     suspend fun loadAll(){
         try{
@@ -40,20 +39,10 @@ object MickiewiczWL {
                 poemList = response.body()?:poemList
             }
             else{
-                poemList = listOf<MickiewiczPoem>(MickiewiczPoem(
-                    title = "Error! Could not get data from Wolne Lektury API."
-                )) //so that the app can continue even without the poems, it ain't very critical
-                Log.e("WL", "Could not get the data from Wolne Lektury API")
+                poemList = listOf<MickiewiczPoem>(MickiewiczPoem())
             }
         } catch (e: Exception){
-            poemList = listOf<MickiewiczPoem>(MickiewiczPoem(
-                title = "Error! Failed to connect to Wolne Lektury API."
-            )) //so that the app can continue even without the poems, it ain't very critical
-            Log.e("WL", e.message.toString())
+            Error(e.message) //here to see the error message with debuger
         }
-    }
-
-    fun updateSelectedPoem(){
-        selectedPoem = poemList.random()
     }
 }
