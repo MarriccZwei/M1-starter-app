8eb3f264468b81c3f3ea3c7e29016d17e6a3b465
MarriccZwei <marricc7@gmail.com>
2025-10-02 18:07:28 -0700

    Revert "working retrofit"
    This reverts commit e496dd672ac3bec151aacd59972c77c4dec97572.

--- frontend/app/src/main/java/com/cpen321/usermanagement/data/remote/wl/MickiewiczInterface.kt
+++
@@ -1,9 +0,0 @@
-package com.cpen321.usermanagement.data.remote.wl
-
-import retrofit2.Response
-import retrofit2.http.GET
-
-interface MickiewiczInterface {
-    @GET("authors/adam-mickiewicz/kinds/liryka/parent_books/")
-    suspend fun getAllPoems(): Response<List<MickiewiczPoem>>
-}
--- frontend/app/src/main/java/com/cpen321/usermanagement/data/remote/wl/MickiewiczPoem.kt
+++
@@ -1,6 +0,0 @@
-package com.cpen321.usermanagement.data.remote.wl
-
-data class MickiewiczPoem(
-    val title:String = "Wolne Lektury",
-    val url:String = "https://wolnelektury.pl/"
-)
